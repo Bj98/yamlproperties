d1a675183333a13de13cee73511c1715d79ba680
Bj98 <bijaykhanal999@gmail.com>
2020-04-25 00:47:01 +0545

    first commit

---
+++ src/main/java/bijay/np/yamlproperties/YamlpropertiesApplication.java
@@ -0,0 +1,13 @@
+package bijay.np.yamlproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlpropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlpropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bijay/np/yamlproperties/YamlpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package bijay.np.yamlproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamlpropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
